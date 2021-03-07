7bf30a94c337fb844d126f06b24bd03be37bfc2f
Ishan Goel <igoel.mail@gmail.com>
2021-03-07 20:01:54 +0400

    ready for ship

--- dl.go
+++ dl.go
@@ -22,26 +22,25 @@ import (
 var (
 	version = "dev"
 	helpMsg = `Dl - Print, download or copy website content
-Usage: dl [-n/--no-overwrite] [-p/--print | -c/--copy] <url>
-       dl [-h/--help | --version/-v]
-
-Without any options, Dl downloads to a file in the working directory.
-When downloading data, Dl shows a progress bar on stderr.
-If the url does not have an http or https protocol specified, Dl will try https
-and then http.
-
+Usage: 
+   dl [-n/--no-overwrite] [-p/--print | -c/--copy] <url>
+   dl [-h/--help | --version/-v]
 Options:
-      -o, --no-overwrite   don't overwrite existing files
-      -p, --print          print downloaded data on stdout
-      -c, --copy           copy data to clipboard (needs one of xclip, xsel, 
-                           wl-copy or termux-clipboard-set installed on Linux)
+   -o, --no-overwrite   don't overwrite existing files
+   -p, --print          print downloaded data on stdout
+   -c, --copy           copy data to clipboard (needs one of xclip, xsel,
+                        wl-copy or termux-clipboard-set installed on Linux)
 Examples:
-       dl google.com
-       dl http://foo.com
-       dl golang.org/dl/go1.16.src.tar.gz
-       dl -c raw.githubusercontent.com/quackduck/dl/main/dl.go
-       dl -p raw.githubusercontent.com/octocat/Hello-World/master/README
-`
+   dl google.com
+   dl http://foo.com
+   dl golang.org/dl/go1.16.src.tar.gz
+   dl -c raw.githubusercontent.com/quackduck/dl/main/dl.go
+   dl -p https://raw.githubusercontent.com/octocat/Hello-World/master/README
+Note:
+   Without any options, Dl downloads to a file in the working directory
+   When downloading data, Dl shows a progress bar on stderr
+   If the url doesn't have an http/https prefix Dl will try https and then http
+   Dl will follow up to 10 redirects`
 	allowOverwrite = true
 )
 
